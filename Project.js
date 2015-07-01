441f574e4b0f0957bc7ebe535d5a41ff1dab36f8
Willliang918 <williamliang918@gmail.com>
2015-07-01 19:03:39 -0400

    updated maps app

--- Project.js
+++ Project.js
@@ -12,67 +12,58 @@ function initialize() {
 	directionsDisplay = [];
 	request = [];
 	selectedMode = [];
+	searchBoxMarkers = [];
+	searchinfoWindow = [];
+	markerD = [];
+	selectedMarker = [];
+	markerAddress = [];
+	inputSelected = false;
 
-	var mapOptions = {
-		center: new google.maps.LatLng(-33.8688, 151.2195),
-		//zoom: 13
-	};
-
-	map = new google.maps.Map(document.getElementById('map-canvas'),
-		mapOptions);
+	map = new google.maps.Map($('map-canvas'));
 
 	var defaultBounds = new google.maps.LatLngBounds(
 		new google.maps.LatLng(40.4774, -74.2591),
 		new google.maps.LatLng(40.9176, -73.7003));
 	map.fitBounds(defaultBounds);
-		
-	var options = {
-		bounds: defaultBounds,
-		componentRestrictions: "us"
-	};
 	
-	for (var i = 0; i < 3; i++){
+	for (var i = 0; i < 4; i++){
 		inputArray[i] = "search"+i;
 		autocompleteArray[i] = new google.maps.places.Autocomplete($(inputArray[i]));
 		autocompleteArray[i].bindTo('bounds', map);
 		directionsDisplay[i] = new google.maps.DirectionsRenderer({"draggable":true, suppressMarkers: true});
 		directionsDisplay[i].setMap(map);
-		//directionsDisplay[i].setPanel($("directions-panel"));
 		infowindow[i] = new google.maps.InfoWindow();
 		marker[i] = new google.maps.Marker({
 			map: map,
 			anchorPoint: new google.maps.Point(0, -29)
 		});
 	};
-	
 	google.maps.event.addListener(autocompleteArray[0], 'place_changed', function() {
-		calcSearchBox(0);
+		calcSearch(0);
 	});
 	google.maps.event.addListener(autocompleteArray[1], 'place_changed', function() {
-		calcSearchBox(1);
+		calcSearch(1);
 	});
 	google.maps.event.addListener(autocompleteArray[2], 'place_changed', function() {
-		calcSearchBox(2);
+		calcSearch(2);
 	});
-/*
-	google.maps.event.addListener(autocompleteArray[3], 'place_changed', function() {
-		calcSearchBox(3);
-	});*/
+	service = new google.maps.places.PlacesService(map);
 }
 
-var calcSearchBox = function(x){
+var calcSearch = function(x){
 	
 	infowindow[x].close();
 	marker[x].setVisible(false);
 	var place = autocompleteArray[x].getPlace();
+
 	if (place.length == 0){
 		return;
 	}
-	currentPlace[x] = place;
-	if (!place.geometry) {
-		window.alert("Autocomplete's returned place contains no geometry");
-		return;
+	if (place.name == ""){
+
+		calcRoute();
 	}
+	currentPlace[x] = place;
 
 	marker[x].setIcon({
 		url: place.icon,
@@ -97,26 +88,32 @@ var calcSearchBox = function(x){
 
 	bounds = new google.maps.LatLngBounds();	
 	for (var i in currentPlace){
-		console.log(currentPlace);
 		bounds.extend(currentPlace[i].geometry.location);
 	};
 	map.fitBounds(bounds);
+	calcRoute();
 
-}
+};
 
 function calcRoute(){
+	directionsDisplay[0].setMap(null);
+	directionsDisplay[1].setMap(null);
+	directionsDisplay[2].setMap(null)
+	
 	var start = 0;
 	var end = 0;
 	var mode = "mode0"
-	//var selectedMode = $("mode").value;
 
 	for (var i = 0; i < 2; i++){
+		
 		if ($(inputArray[i]).value != ""){
 			start = $(inputArray[i]).value;
+			console.log(start);
 			mode = selectedMode[i] = "mode"+i;
 		}
 		if ($(inputArray[i+1]).value != null){
 			end = $(inputArray[i+1]).value; 
+			console.log(end);
 		} 
 		
 		request[i] = { 
@@ -130,27 +127,164 @@ function calcRoute(){
 		if (status == google.maps.DirectionsStatus.OK){
 		directionsDisplay[0].setOptions({ preserveViewport:true});
 		directionsDisplay[0].setDirections(response);
+		directionsDisplay[0].setMap(map);
 	}});
 	directionsService.route(request[1], function(response, status){
 		if (status == google.maps.DirectionsStatus.OK){
 			directionsDisplay[1].setOptions({ preserveViewport:true});
 			directionsDisplay[1].setDirections(response);
+			directionsDisplay[1].setMap(map);
 	}});
-	/*
 	directionsService.route(request[2], function(response, status){
 		if (status == google.maps.DirectionsStatus.OK){
+			directionsDisplay[2].setOptions({ preserveViewport:true});
 			directionsDisplay[2].setDirections(response);
-	}});*/
-	
-	map.fitBounds(bounds);
+			directionsDisplay[2].setMap(map);
+	}});
 }
 
+detourMarkers = [];
 
+var searchButton = function() {
+	inputSelected = false;
+	$("clear").click;
+	for (var i in detourMarkers) {
+		detourMarkers[i].setMap(null);
+    };
+	for (var i in markerAddress) {
+		markerAddress[i].setMap(null);
+    };
+	console.log(detourMarkers);
+	detourMarkers = [];
+	console.log(detourMarkers);
+	query = $("SearchBoxTest").value
+	var request = {
+		bounds: map.getBounds(),
+		query: query,
+
+	};
+	service.textSearch(request, callback);
+
+};
+function callback(results, status){
+	if (status == google.maps.places.PlacesServiceStatus.OK) {
+		for (var i = 0; i < results.length; i++) {
+
+			if (i > 9){
+				break;
+			} else {
+				(function(detourMarkers, markerAddress, selectedMarker){
+
+					var place = results[i];
+					var markerD = new google.maps.Marker({
+						map: map,
+						title: place.name,
+						position: place.geometry.location,
+					});
+					searchinfoWindow = new google.maps.InfoWindow();
+					google.maps.event.addListener(markerD, 'mouseover', function() {
+						markerAddress = [];
+						markerAddress.push(place.name);
+						markerAddress.push(place.formatted_address);
+						markerAddress.push(place);
+						$("searchx").value = markerAddress[0] + "\n" + markerAddress[1];
+						$("search1").value = markerAddress[1];
+						searchinfoWindow.setContent(place.name);
+						searchinfoWindow.open(map, markerD);
+						//calcRoute();
+					});
+					
+					google.maps.event.addListener(markerD, 'mouseout', function() {
+						if(inputSelected = false){
+							$("search1").value = "";
+						};
+						$("searchx").value = "";
+						//$("search1").value = "";
+						searchinfoWindow.close();
+			
+					});
+					google.maps.event.addListener(markerD, 'click', function() {
+
+						$("clear").onclick = function (){
+							for (var i in detourMarkers) {
+								detourMarkers[i].setMap(null);
+							};
+							detourMarkers = [];
+							$("search1").value = "";
+							calcRoute();
+						};
+						
+						inputSelected = true;		
+						marker[1].setVisible(false);
+						infowindow[1].close();						
+						var place = markerAddress[2];	
+						var markerD = new google.maps.Marker({
+							map: map,
+							//icon: image,
+							title: place.name,
+							position: place.geometry.location,
+						});
+						
+						searchinfoWindow = new google.maps.InfoWindow();
+						console.log(results.name);
+						google.maps.event.addListener(markerD, 'mouseover', function() {
+							markerAddress = [];
+							markerAddress.push(place.name);
+							markerAddress.push(place.formatted_address);
+							markerAddress.push(place);
+							$("searchx").value = markerAddress[0] + "\n" + markerAddress[1];
+							console.log(markerAddress);
+							searchinfoWindow.setContent(markerAddress[0] + "<br>" + markerAddress[1]);
+							searchinfoWindow.open(map, markerD);
+						});
+						
+						google.maps.event.addListener(markerD, 'mouseout', function() {
+						$("searchx").value = "";
+						searchinfoWindow.close();					
+						});
+						
+						console.log(markerAddress);
+						$("search1").value = markerAddress[1];
+						for (var i in detourMarkers) {
+							detourMarkers[i].setMap(null);
+						};
+						detourMarkers = [];
+						detourMarkers.push(markerD);
+						console.log(detourMarkers);	
+						calcRoute();						
+					});
+					
+					$("clear").onclick = function (){
+						for (var i in detourMarkers) {
+							detourMarkers[i].setMap(null);
+						};
+						detourMarkers = [];
+					};
+
+					detourMarkers.push(markerD);
+					console.log(detourMarkers);
+						
+				}(detourMarkers, markerAddress, selectedMarker));
+
+			};
+		};
+	};
+};
+
+
+var clearButton = function (){
+	for (var i in detourMarkers) {
+		detourMarkers[i].setMap(null);
+	};
+	detourMarkers = [];
+};
 var routeButton = function callcalcRoute(){
 	calcRoute();
 }
 window.onload = function(){
 	 $("route").onclick = routeButton;
+	 $("search").onclick = searchButton;
+	 $("clear").onclick = clearButton;
 };
 
 google.maps.event.addDomListener(window, 'load', initialize);
